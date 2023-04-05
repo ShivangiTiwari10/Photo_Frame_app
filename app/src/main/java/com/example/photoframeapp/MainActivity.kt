f4f01dced844aad2789633a41708065b595deaad
Shivangi <shivangit1111@gmail.com>
2023-04-05 13:20:25 +0530

    feat: view bind

--- app/src/main/java/com/example/photoframeapp/MainActivity.kt
+++ app/src/main/java/com/example/photoframeapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.photoframeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.photoframeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

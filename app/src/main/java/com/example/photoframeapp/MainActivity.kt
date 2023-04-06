f85adcc0a030ad36c8e97c266e7fcb4e9d544d2c
Shivangi <shivangit1111@gmail.com>
2023-04-06 10:59:48 +0530

    feat: change images onButton clicked

--- app/src/main/java/com/example/photoframeapp/MainActivity.kt
+++ app/src/main/java/com/example/photoframeapp/MainActivity.kt
@@ -1,16 +1,47 @@
 package com.example.photoframeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ViewFlipper
+import androidx.appcompat.app.AppCompatActivity
 import com.example.photoframeapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var viewFlipper: ViewFlipper? = null
+    var next: ImageButton? = null
+    private var previous: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewFlipper = binding.viewFlipper
+        next = binding.next
+        previous = binding.previous
+
+        binding.next.setOnClickListener {
+
+            onClicked(next!!)
+        }
+
+        binding.previous.setOnClickListener {
+            onClicked(previous!!)
+        }
+    }
+
+    private fun onClicked(view: View) {
+        if (view == next) {
+            viewFlipper?.showNext()
+        } else if (view == previous) {
+            viewFlipper?.showPrevious()
+        }
     }
 }
+
+
+

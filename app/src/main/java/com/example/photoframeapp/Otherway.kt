bb278bc19e8a7b575c1b32485a990a3f65292692
Shivangi <shivangit1111@gmail.com>
2023-04-06 14:20:42 +0530

    chore: add new activity

---
+++ app/src/main/java/com/example/photoframeapp/Otherway.kt
@@ -0,0 +1,11 @@
+package com.example.photoframeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Otherway : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otherway)
+    }
+}

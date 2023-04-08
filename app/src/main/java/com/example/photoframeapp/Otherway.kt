f63c78c8c4ae48da1a575e7b65ab30a9f997d843
Shivangi <shivangit1111@gmail.com>
2023-04-08 17:25:39 +0530

    feat: implement binding

--- app/src/main/java/com/example/photoframeapp/Otherway.kt
+++ app/src/main/java/com/example/photoframeapp/Otherway.kt
@@ -2,10 +2,15 @@ package com.example.photoframeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.photoframeapp.databinding.ActivityOtherwayBinding
 
 class Otherway : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOtherwayBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_otherway)
+
+        binding = ActivityOtherwayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

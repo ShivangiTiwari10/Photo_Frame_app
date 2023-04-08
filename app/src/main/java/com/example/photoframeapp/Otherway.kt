8861b6ebe64a05c51128aa638e0d29ac80a71a8a
Shivangi <shivangit1111@gmail.com>
2023-04-08 17:28:29 +0530

    feat: hide actionBar

--- app/src/main/java/com/example/photoframeapp/Otherway.kt
+++ app/src/main/java/com/example/photoframeapp/Otherway.kt
@@ -6,11 +6,15 @@ import com.example.photoframeapp.databinding.ActivityOtherwayBinding
 
 class Otherway : AppCompatActivity() {
 
+       var currentImage = 0
+
     private lateinit var binding: ActivityOtherwayBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityOtherwayBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
     }
 }

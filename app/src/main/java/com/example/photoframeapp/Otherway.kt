8fdfc2d100b4e723bdcfaacee3bee7363484c81d
Shivangi <shivangit1111@gmail.com>
2023-04-09 17:21:22 +0530

    feat: showing image on button clicked

--- app/src/main/java/com/example/photoframeapp/Otherway.kt
+++ app/src/main/java/com/example/photoframeapp/Otherway.kt
@@ -1,14 +1,19 @@
 package com.example.photoframeapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.photoframeapp.databinding.ActivityOtherwayBinding
 
 class Otherway : AppCompatActivity() {
 
-       var currentImage = 0
+    private var currentImage = 0
+    private lateinit var image: ImageView
 
     private lateinit var binding: ActivityOtherwayBinding
+
+    @SuppressLint("DiscouragedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,5 +21,49 @@ class Otherway : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        binding.imgPrev.setOnClickListener {
+
+            val idCurrentImgString = "pic$currentImage"
+
+//            convert string id into integer address associated with it
+            val idCurrentImgInt =
+                this.resources.getIdentifier(idCurrentImgString, "id", packageName)
+
+            image = findViewById(idCurrentImgInt)
+            image.alpha = 0f
+
+
+            currentImage = (4+currentImage-1)%4
+            val idImgToShowString = "pic$currentImage"
+//            convert string id into integer address associated with it
+            val idImgToShowInt =
+                this.resources.getIdentifier(idImgToShowString, "id", packageName)
+
+
+            image = findViewById(idImgToShowInt)
+            image.alpha = 1f
+        }
+
+        binding.imgNext.setOnClickListener {
+
+            val idCurrentImgString = "pic$currentImage"
+
+//            convert string id into integer address associated with it
+            val idCurrentImgInt =
+                this.resources.getIdentifier(idCurrentImgString, "id", packageName)
+
+            image = findViewById(idCurrentImgInt)
+            image.alpha = 0f
+
+            currentImage = (4+currentImage+1)%4
+            val idImgToShowString = "pic$currentImage"
+//            convert string id into integer address associated with it
+            val idImgToShowInt =
+                this.resources.getIdentifier(idImgToShowString, "id", packageName)
+
+            image = findViewById(idImgToShowInt)
+            image.alpha = 1f
+        }
     }
 }
